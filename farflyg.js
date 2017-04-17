7591127ea4804009642fb374d7ab09f7d4cad77b
Love <babyfaceninja1337@gmail.com>
2017-04-17 16:16:11 +0200

    Remade the header and started on the menu

---
+++ farflyg.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#showSearchbar").click(function(){
+       $("#searchText").show('slow'); 
+    });
+    $("#slideMenu").click(function(){
+       $("#menu").slideToggle("slow"); 
+    });
+});
